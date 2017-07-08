fe826d6a62eb047a8d994429ef5ac84d0ecba3b2
charanjitghai <ghaicharanjit@gmail.com>
2017-07-08 14:33:01 -0700

    Client Code added

--- Code/Main.py
+++ Code/Main.py
@@ -6,24 +6,27 @@ import os
 import shutil
 import xml.etree.ElementTree as ET
 
-templatize = Templatize(debug=True, objects=sys.argv[1].split(','), mds_path=Config.mds_path, patterns=sys.argv[2].split(','))
-out_dir = sys.argv[3]
-
+"""
+cd Opportunity/ 
+object.properties
+"""
+#Opportunity,Lead /Users/cghai/Documents/Code/BaselineTemplating/ActiveMDS VO.xml.xml,EO.xml.xml
+templatize = Templatize(debug=True, objects=sys.argv[1].split(','), mds_path=sys.argv[2], patterns=sys.argv[3].split(','))
+root_dir = os.getcwd()
 for pattern in templatize.patterns:
-    os.chdir(out_dir)
+    os.chdir(root_dir)
     cluster = ClusterManager.get_cluster(pattern)
     cluster.get_template()
-    dir_name = cluster.get_name().replace('.','_')
-    shutil.rmtree(dir_name, ignore_errors=True)
-    os.mkdir(dir_name)
-    os.chdir(out_dir + "/" + dir_name)
+    pattern_dir = cluster.get_name().replace('.','_')
     artifacts = cluster.get_artifacts()
     template = artifacts[0].get_template()
     template.write('SmartBaseline.xml')
     for artifact in cluster.get_artifacts():
         tokens = artifact.get_unique_tokens()
-        file_name = artifact.get_obj() + '.properties'
-        f = open(file_name, 'w')
+        obj_dir = root_dir + '/' + artifact.get_obj()
+        os.chdir(obj_dir)
+        file_name = 'object.properties'
+        f = open(file_name, 'a')
         for token in sorted(tokens.keys()):
             f.write(token)
             f.write("=")
